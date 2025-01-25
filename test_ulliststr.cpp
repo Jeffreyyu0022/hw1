d6eb726c6551f1e11e7c96a1d024d714f8cc5417
Jeffrey Yu <yujeffre@usc.edu>
2025-01-25 23:25:05 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <iostream>
 #include <sstream>
+#include <iostream>
+using namespace std;
 
 #include "ulliststr.h"
 
@@ -9,5 +11,48 @@
 
 int main(int argc, char* argv[])
 {
+    ULListStr dat;
 
+    cout << "Is empty: " << dat.empty() << endl;  // Should be 1 aka true
+    cout << "Size: " << dat.size() << endl;       // Should be 0
+
+    dat.push_back(to_string(7));
+    dat.push_front(to_string(8));
+    dat.push_back(to_string(9));
+    cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << endl;
+    // prints: 8 7 9
+    cout << "Amount of Strings: " << dat.size() << endl;  
+
+    cout << "Front: " << dat.front() << endl;
+    cout << "Back: " << dat.back() << endl;
+
+    cout << "Remove all using pop_back:" << endl;
+    while (!dat.empty()) {
+        cout << "Remove: " << dat.back() << endl; // Print the current back element
+        dat.pop_back(); // Remove the back element
+    }
+    cout << "Updated size: " << dat.size() << endl;
+
+    cout << "Adding with push_front:" << endl;
+    for (size_t i = 0; i < 8; i++) {
+        dat.push_front(to_string(i)); // Add element to the front
+    }
+
+    // Print the list in the correct order
+    for (size_t i = 0; i < dat.size(); i++) {
+        cout << dat.get(i) << " ";
+    }
+    cout << endl;
+    cout << "Amount of Strings: " << dat.size() << endl; 
+
+    cout << "Remove all using pop_front:" << endl;
+
+    while (!dat.empty()) {
+        cout << "Remove: " << dat.front() << endl; // Print the current front element
+        dat.pop_front(); // Remove the front element
+    }
+
+    cout << "Deleteing an empty list using pop_front:" << endl;
+    dat.pop_front(); // Will delete the node
+    cout << "Will display if there is no segfault" << endl;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,135 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+void ULListStr::push_back(const std::string& val)
+{
+  // Use back func to see find the value
+  if(tail_ == nullptr || tail_->last == ARRSIZE){
+    Item* newItem = new Item();
+    if(tail_ != nullptr){
+      tail_->next = newItem;
+      newItem->prev = tail_;
+    }
+    else{
+      head_ = newItem;
+    }
+    //newItem->next = nullptr;
+    tail_ = newItem;
+  }
+  tail_->val[tail_->last] = val;
+  tail_->last++;
+  size_++;
+}
+
+void ULListStr::pop_back()
+{
+  // Use back func to see find the value
+  if(head_ == nullptr || tail_ == nullptr){
+    return;
+  }
+
+  if (tail_->last == 0) { // If the tail is empty, deallocate it 
+    Item* temp = tail_;
+    tail_ = tail_->prev;
+    if (tail_ != nullptr) {
+        tail_->next = nullptr;
+    }
+    else {
+      head_ = nullptr;
+    }
+    delete temp;
+  }
+	
+	tail_->last--;
+  size_--;
+}
+
+std::string const & ULListStr::back() const
+{
+  return tail_->val[tail_->last - 1];
+}
+
+/**
+ * Adds a new value to the front of the list.
+ * If there is room before the 'first' value in
+ * the head node add it there, otherwise, 
+ * allocate a new head node.
+ *   - MUST RUN in O(1)
+ */
+void ULListStr::push_front(const std::string& val)
+{
+  // Empty List
+  if(head_ == nullptr || head_->first == 0){
+    Item* newItem = new Item();
+    newItem->last = ARRSIZE; // Initialize last to ARRSIZE for reverse indexing
+    newItem->first = ARRSIZE - 1; // Insert at the back of the new head
+    newItem->val[ARRSIZE - 1] = val;
+
+    newItem->next = head_;
+    if (head_ != nullptr) {
+      head_->prev = newItem;
+    }
+    head_ = newItem;
+
+    if (tail_ == nullptr) { // If list was empty, tail points to head
+      tail_ = head_;
+    }
+  } 
+  else {
+    // If there is space in the current head node
+    head_->first--;
+    head_->val[head_->first] = val;
+  }
+  size_++;
+}
+
+/**
+ * Removes a value from the front of the list
+ *   - MUST RUN in O(1)
+ */
+void ULListStr::pop_front()
+{
+  if(head_ == nullptr){
+    return;
+  }
+
+  if (head_->first == head_->last) { // If the node is empty
+    Item* temp = head_;
+    head_ = head_->next;
+    if (head_ != nullptr) {
+      head_->prev = nullptr;
+    } 
+    else { 
+      tail_ = nullptr;
+    }
+    delete temp;
+  }
+
+	head_->first++; // Move the front index forward
+  size_--;
+}
+
+std::string const & ULListStr::front() const
+{
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+  if(loc >= size_){
+    return nullptr;
+  }
+
+  size_t index = loc;
+  Item* current = head_;
+  while (current != nullptr) {
+    size_t node_size = current->last - current->first;
+    if (index < node_size) {
+      return &current->val[current->first + index];
+    }
+    index -= node_size;
+    current = current->next;
+  }
+  return nullptr;
+}
